aef6e45a16b8a8f8455a116bf07aaec6c15056de
JK <vandung31141@gmail.com>
2022-07-17 21:07:30 +0700

    add more validations, fix UI

--- src/helper/Text.js
+++ src/helper/Text.js
@@ -19,5 +19,6 @@ export const errorText={
     phone2: "Số điện thoại không phải là số Việt Nam hợp lệ (+84)",
     password: "Mật khẩu phải từ 6 - 20 ký tự, và bao gồm các ký tự chữ hoa, thường, số và ký tự đặc biệt",
     id_card: "Phải bao gồm đủ 9-12 ký tự số",
-    email: "Sai cấu trúc email"
+    email: "Sai cấu trúc email",
+    space: "Ký tự đầu tiên không được để dấu cách!"
 }
