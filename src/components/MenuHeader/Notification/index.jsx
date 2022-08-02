a9621c9f9316c3ffd25e301664370bad1762e50b
JK <vandung31141@gmail.com>
2022-08-02 09:33:29 +0700

    add dropdown header menu

--- src/components/MenuHeader/Notification/index.jsx
+++ src/components/MenuHeader/Notification/index.jsx
@@ -5,65 +5,121 @@ import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
 // import { useAppDispatch, useAppSelector } from "../../../hook/useRedux";
 // import { actions } from "../../../redux";
 import "./index.scss";
-import { Button, Form, Space, Input } from "antd";
+import { Form, Space, Input } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
 import { CloseCircleOutlined } from "@ant-design/icons";
 import "antd/lib/style/themes/default.less";
 import "antd/dist/antd.less";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { IconButton } from "@mui/material";
+import moment from "moment";
 
-function Notification() {
+function Notification(props) {
   const [items, setItems] = useState([]);
   const [inputItem, setInputItem] = useState([]);
+  const [update, setUpdate] = useState(false);
+  useEffect(() => {
+    const temp = JSON.parse(localStorage.getItem("items"));
+    if (temp.length > 0) {
+      temp.reverse();
+      setItems(temp);
+    }
+  }, []);
+
   useEffect(() => {
     localStorage.setItem("items", JSON.stringify(items));
-  }, [items]);
+  }, [items, update]);
 
   function addItem() {
-    setItems(items.push(inputItem));
+    let temp = items;
+    let current = new Date();
+    const obj = { name: inputItem, time: current.toString() };
+    if (temp.length >= 3) {
+      temp[0] = obj;
+    } else {
+      temp.push(obj);
+    }
+    setItems(temp);
+    // localStorage.setItem("items", JSON.stringify(items));
+
+    console.log(items.length);
+    setUpdate(!update);
+    setInputItem("");
+  }
+  function deleteItem(e) {
+    const result = items.filter((item) => item.time !== e.time);
+    setItems(result);
+    setUpdate(!update);
   }
   return (
     <>
       {/* TẠO THÔNG BÁO */}
-      <div class="notificationCont">
-        <div class="inputCont">
-          <div class="titleNote">
-            <h2>TẠO THÔNG BÁO MỚI</h2>
-            <CloseOutlined />
+      <div className="notificationCont" {...props}>
+        <div className="inputCont">
+          <div className="titleNote">
+            <h3>TẠO THÔNG BÁO MỚI</h3>
           </div>
-          <Input
+
+          <TextField
+            sx={{ textarea: { color: "#fff" } }}
+            multiline
+            maxRows={6}
+            variant="standard"
             value={inputItem}
             onChange={(e) => setInputItem(e.target.value)}
             placeholder="Nội dung thông báo"
           />
-          <div class="createNote">
-            <Button type="primary" onClick={addItem}>
+
+          <div className="createNote">
+            <Button
+              color="success"
+              size="large"
+              variant="contained"
+              onClick={addItem}
+              sx={{ color: "#fff" }}
+            >
               Tạo thông báo
             </Button>
           </div>
         </div>
-        <div class="mainNote">
-          <div class="titleMain">
-            <CloseCircleOutlined />
-            <h3>Hết sữa rồi nhé</h3>
-            <CloseOutlined />
-          </div>
-          <div class="timeCont">
-            <h5>Today</h5>
-            <h5>10:30PM</h5>
-          </div>
-        </div>
+        {items.length > 0
+          ? items.map((item) => {
+              return (
+                <div key={item.name} className="mainNote">
+                  <div className="titleMain">
+                    <div className="leftTitle">
+                      <CloseCircleOutlined />
+                      <div className="title"> {item.name}</div>
+                    </div>
+                    <IconButton
+                      color="info"
+                      size="small"
+                      onClick={() => deleteItem(item)}
+                    >
+                      <CloseOutlined />
+                    </IconButton>
+                  </div>
+                  <div className="timeCont">
+                    <span>{moment(item.time).format("DD/MM/YYYY")}</span>
+                    <br />
+                    <span> {moment(item.time).format(" h:mma")}</span>
+                  </div>
+                </div>
+              );
+            })
+          : null}
       </div>
-
-      {/* XÓA THÔNG BÁO */}
-      <form class="notificationErase">
-        <form class="contentErase">
+      {/* XÓA THÔNG BÁO
+      <form className="notificationErase">
+        <form className="contentErase">
           <Form.Item>
             <h2>XÓA THÔNG BÁO</h2>
-            <div class="contentImpor">
+            <div className="contentImpor">
               <CloseCircleOutlined />
               <h4>Hết sữa rồi nhé</h4>
             </div>
-            <Space class="buttonErase">
+            <Space className="buttonErase">
               <Button type="primary" htmlType="submit">
                 Xóa
               </Button>
@@ -71,7 +127,7 @@ function Notification() {
             </Space>
           </Form.Item>
         </form>
-      </form>
+      </form> */}
     </>
   );
 }
