3f8505d307fddac09a7e046ed673cfef5bf250d0
197PM21988 - NGUYỄN HỒNG THỊNH - K25T-PM1 <thinh.197pm21988@vanlanguni.vn>
2022-08-09 00:17:37 +0700

    edit--UI-analysis

--- src/pages/admin/Analysis/index.jsx
+++ src/pages/admin/Analysis/index.jsx
@@ -15,29 +15,40 @@ import { Line } from "@ant-design/charts";
 
 import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined';
 import ArrowDownwardOutlinedIcon from '@mui/icons-material/ArrowDownwardOutlined';
+import { CloseOutlined } from "@ant-design/icons";
+import { IconButton } from "@mui/material";
 
 const { Search } = Input;
+const { Option } = Select;
 const labels = {
     statistic: "Báo cáo thống kê doanh thu",
     activity_summary: "Tóm tắt hoạt động",
     bank_card: "Thẻ ngân hàng của tôi",
     target: "Mục tiêu",
+    morning_money: "Tiền sáng",
+    lunch_money: "Tiền trưa",
+    evening_money: "Tiền tối",
 };
+function getHeaderTitle() {
+    return "Danh sách thống kê/ngày";
+  }
 const data = [
-    { year: "1991", value: 3 },
-    { year: "1992", value: 4 },
-    { year: "1993", value: 3.5 },
-    { year: "1994", value: 5 },
-    { year: "1995", value: 4.9 },
-    { year: "1996", value: 6 },
-    { year: "1997", value: 7 },
-    { year: "1998", value: 9 },
-    { year: "1999", value: 13 },
+    { month: "1", value: 3 },
+    { month: "2", value: 4 },
+    { month: "3", value: 3.5 },
+    { month: "4", value: 5 },
+    { month: "5", value: 4.9 },
+    { month: "6", value: 6 },
+    { month: "7", value: 7 },
+    { month: "8", value: 9 },
+    { month: "9", value: 13 },
+    { month: "10", value: 13 },
+    { month: "11", value: 13 },
+    { month: "12", value: 13 },
 ];
 const config = {
     data,
-    height: 400,
-    xField: "year",
+    xField: "month",
     yField: "value",
     point: {
         size: 5,
@@ -70,7 +81,7 @@ export default function Analysis() {
                 <div className="cards">
                     <Card sx={{ width: "50%", marginRight: "2%" }}>
                         <CardContent>
-                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+                            <Typography sx={{ fontSize: 16 }} gutterBottom>
                                 Tổng doanh thu
                             </Typography>
                             <Typography sx={{ fontSize: 28 }}>
@@ -78,7 +89,7 @@ export default function Analysis() {
                             </Typography>
                         </CardContent>
                         <CardActions>
-                            <div className="cardCont">
+                            <div className="cardConts">
                                 12% Increase From Target
                                 <ArrowUpwardOutlinedIcon />
                             </div>
@@ -86,7 +97,7 @@ export default function Analysis() {
                     </Card>
                     <Card sx={{ width: "50%", marginRight: "2%" }}>
                         <CardContent>
-                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+                            <Typography sx={{ fontSize: 16 }} gutterBottom>
                                 Ví điện tử
                             </Typography>
                             <Typography sx={{ fontSize: 28 }}>
@@ -94,7 +105,7 @@ export default function Analysis() {
                             </Typography>
                         </CardContent>
                         <CardActions>
-                            <div className="cardCont">
+                            <div className="cardConts">
                                 2% Decrease From Target
                                 <ArrowDownwardOutlinedIcon />
                             </div>
@@ -110,7 +121,7 @@ export default function Analysis() {
                             </Typography>
                         </CardContent>
                         <CardActions>
-                            <div className="cardCont">
+                            <div className="cardConts">
                                 6% Increase From Target
                                 <ArrowUpwardOutlinedIcon />
                             </div>
@@ -126,7 +137,7 @@ export default function Analysis() {
                             </Typography>
                         </CardContent>
                         <CardActions>
-                            <div className="cardCont">
+                            <div className="cardConts">
                                 1% Increase From Target
                                 <ArrowUpwardOutlinedIcon />
                             </div>
@@ -167,11 +178,135 @@ export default function Analysis() {
                 </div>
                 <div>
                     <h2>{labels.activity_summary}</h2>
+                   
                 </div>
+                <div className="modalCont">
+                        {/* {modalError && <AlertModal chilren={errorText.formValidation} />} */}
+                        <div className="headerCont">
+                            <h2>{getHeaderTitle()}</h2>
+                            <IconButton >
+                                <CloseOutlined />
+                            </IconButton>
+                        </div>
+                        <Form  className="form" initialValues={{ modifier: "public" }}>
+                            <div className="bodyCont">
+                                <div style={{ width: "40%" }}>
+                                    <h4>{labels.morning_money}</h4>
+                                    <Form.Item
+                                        name=""
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: `Không được để trống tiền buổi sáng`,
+                                            },
+                                            {
+                                                pattern: new RegExp(/^\w/),
+                                                // message: errorText.space,
+                                            },
+                                        ]}
+                                    >
+                                        <Select
+                                            // disabled={isDetail}
+                                            dropdownStyle={{ zIndex: 2000 }}
+                                            placeholder="Nhập tiền buổi sáng"
+                                        >
+                                            <Option value={true}>100.000 VND</Option>
+                                            <Option value={false}>200.000 VND</Option>
+                                        </Select>
+                                    </Form.Item>
+                                    <h4>{labels.lunch_money}</h4>
+                                    <Form.Item
+                                        name=""
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: `Không được để trống tiền buổi trưa`,
+                                            },
+                                            {
+                                                pattern: new RegExp(/^\w/),
+                                                // message: errorText.space,
+                                            },
+                                        ]}
+                                    >
+                                        <Select
+                                            // disabled={isDetail}
+                                            dropdownStyle={{ zIndex: 2000 }}
+                                            placeholder="Nhập tiền buổi trưa"
+                                        >
+                                            <Option value={true}>100.000 VND</Option>
+                                            <Option value={false}>200.000 VND</Option>
+                                        </Select>
+                                    </Form.Item>
+                                    <h4>{labels.evening_money}</h4>
+                                    <Form.Item
+                                        name=""
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: `Không được để trống tiền buổi tối`,
+                                            },
+                                            {
+                                                pattern: new RegExp(/^\w/),
+                                                // message: errorText.space,
+                                            },
+                                        ]}
+                                    >
+                                        <Select
+                                            // disabled={isDetail}
+                                            dropdownStyle={{ zIndex: 2000 }}
+                                            placeholder="Nhập tiền buổi tối"
+                                        >
+                                            <Option value={true}>100.000 VND</Option>
+                                            <Option value={false}>200.000 VND</Option>
+                                        </Select>
+                                    </Form.Item>
+                                </div>
+                            </div>
+                            <div className="btnAnalysis">
+                                <Button
+                                    size="Large"
+                                    color="primary"
+                                    variant="contained"
+                                    style={{
+                                        paddingLeft: "15%",
+                                        paddingRight: "15%",
+                                        paddingTop: "2%",
+                                        paddingBottom: "2%",
+                                        color: "#fff",
+                                    }}
+                                    // disabled={loading}
+                                    // onClick={dataItem && isDetail === true ? editItem : handleOk}
+                                >
+                                    Lưu
+                                </Button>
+                            </div>
+                        </Form>
+                        {/* <AlertDialog
+                            children={`Xác nhận xoá ${dataItem ? dataItem.name : null} ?`}
+                            title="Xoá nhóm món"
+                            onAccept={handleDelete}
+                        /> */}
+                    </div>
             </div>
             <div className="rightAnalysis">
-                <div>
+                <div className="bankCard">
                     <h3>{labels.bank_card}</h3>
+                    <Card sx={{ width: "140%", backgroundColor: "#4CACBA" }}>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 16 }} gutterBottom>
+                                Tổng doanh thu
+                            </Typography>
+                            <Typography sx={{ fontSize: 28 }}>
+                                $ 120,000
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <div className="cardCont">
+                                12% Increase From Target
+                                <ArrowUpwardOutlinedIcon />
+                            </div>
+                        </CardActions>
+                    </Card>
                 </div>
                 <div>
                     <h3>{labels.target}</h3>
