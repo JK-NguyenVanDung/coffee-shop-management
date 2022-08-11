5805b6d16fc0fd4c18f3f8e90465b7dfb85ce596
JK <vandung31141@gmail.com>
2022-08-12 00:45:29 +0700

    fix UI, add font, fix bugs

--- src/api/Collections/analysis.js
+++ src/api/Collections/analysis.js
@@ -3,7 +3,7 @@ const v1 = "/v1/admin";
 
   export const getData = async (props) => {
     try {
-      const response = await api.put(`/statistics`,props.body);
+      const response = await api.post(`/statistics`,props);
       return response;
     } catch (err) {
       throw err.message;
